286c84140502bb7d34f908e0c897c1d4ef0373ac
Emanuel Bierman <emanuel.bierman@gmail.com>
2018-04-18 13:39:34 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -2,9 +2,9 @@
 def speak_to_grandma(speak)
   if speak == 'I LOVE YOU GRANDMA!'
     return 'I LOVE YOU TOO PUMPKIN!'
-  elsif speak.match(/\A.{1,2}[a-z]{1,}.*\A/)
+  elsif speak.match(/\A.{1,2}[a-z]{1,}.*\Z/)
     return 'HUH?! SPEAK UP, SONNY!'
-  elsif speak.match(/\A.{1,2}[A-Z]{1,}.*\A/)
+  elsif speak.match(/\A.{1,2}[A-Z]{1,}.*\Z/)
     return "NO, NOT SINCE 1938!"
   end
 end
